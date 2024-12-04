29b76555a41565bb863554950703f3b5194d1b77
Soumya Chakraborty <soumya.chakraborty@iiml.org>
2024-12-04 21:43:50 +0530

    Create authService for appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        console.error(
+          "Appwrite Service :: createAccount :: userAccount returned Falsy"
+        );
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.error("Appwrite Service :: getCurrentUser :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.error("Appwrite Service :: logout :: ", error);
+    }
+  }
+}
+
+const authAervice = new AuthService();
+export default authAervice;
