b76927b435613f7c4fce905f998412c5d85f4bc0
Soumya Chakraborty <soumya.chakraborty@iiml.org>
2024-12-05 12:13:56 +0530

    Setup the Authentication layer for the pages navigation

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
-function Protected({ children, authenticated = true }) {
-  return <>{children}</>;
+function Protected({ children, authentication = true }) {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    if (authentication && !authStatus) {
+      navigate("/login");
+    } else if (!authentication && authStatus) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authentication, authStatus, navigate]);
+
+  return loader ? (
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
+      <div className="w-16 h-16 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+    </div>
+  ) : (
+    children
+  );
 }
 
 export default Protected;
